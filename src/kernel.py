712069c2b67ac1952d54a121000b9752665148ca
Joshua Nuez <jn2548@columbia.edu>
2016-12-01 20:14:03 -0500

    Adds a similarity kernel.

--- src/kernel.py
+++ src/kernel.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
+from pycuda import driver, compiler, gpuarray, tools
+import pycuda.autoinit
 
-kernel = """
+kernelCUDA = """
+__global__ void similarity(float* x, float* y, float* z, float* similarity, int size) {
+	
+	unsigned int col = blockIdx.x*blockDim.x + threadIdx.x;
+	unsigned int row = blockIdx.y*blockDim.y + threadIdx.y;
+
+	float dist = 0;
+	
+	dist = -(pow((x[row]-x[col]), 2.0) + pow((y[row]-y[col]), 2.0) + pow((z[row]-z[col]), 2.0));
+
+	similarity[row*size+col] = dist;
+	__syncthreads();
+}
+"""
+kernelCL = """
 
 """
