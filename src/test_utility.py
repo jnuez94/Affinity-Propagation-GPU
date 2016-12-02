396dc1f0d2f1fccddc8db07b0848c8c59d221673
Joshua Nuez <jn2548@columbia.edu>
2016-12-01 19:36:44 -0500

    Adds a function that reads point cloud data.

---
+++ src/test_utility.py
@@ -0,0 +1,7 @@
+import utility as ut
+
+x, y, z = ut.readPointCloud('../data/short.xyz')
+
+print "x: ", x
+print "y: ", y
+print "z: ", z
--- src/utility.py
+++ src/utility.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python
 
-# Parameter:
-# Return:
+import numpy as np
+
+# Parameter: filename of the data set
+# Return: x, y, z numpy arrays.
 def readPointCloud(filename):
+	x = []
+	y = []
+	z = []
+	for lines in open(filename).readlines():
+		xi, yi, zi, _, _ = lines.split(' ')
+		x.append(float(xi))
+		y.append(float(yi))
+		z.append(float(zi))
+
+	x = np.copy(x).astype(np.float32)
+	y = np.copy(y).astype(np.float32)
+	z = np.copy(z).astype(np.float32)
 	return x, y, z
 
