0ffaa5b68ddc38589abb2457c2e1a2fbde87469b
Harrison Liew <harrisonliew@gmail.com>
2016-12-07 17:44:18 -0500

    Test convergence

--- src/kernel.py
+++ src/kernel.py
@@ -157,149 +157,33 @@ __global__ void availabilities(float* A, float* R, float* RP, int iteration) {
 		A[j] = (1-%(DAMP)s)*a + %(DAMP)s*a_old; //dampen
 	}
 }
-/*
-// Kernel passes messages. Each block of 1024 threads computes 1 row (N) of A & R.
-__global__ void apupdate(float* S, float* R, float* A) {
-	unsigned int blk_os;
-	unsigned int i;
-	unsigned int j;
-	unsigned int k;
-	float temp = 0; //temp register
-	__shared__ float old[%(N)s]; //temp for previous R or A
-	__shared__ float AS[%(N)s]; //A+S
-	// shared variable that stores our atomic operation results
-	// TODO: need to make it a pointer for atomic operations
-	__shared__ float atom_temp = -FLOAT_MAX;
-	__shared__ int max_idx; //index of maximums
-	//__shared__ bool dn = false; //convergence flag
-
-	//while (!dn) {
-
-		// COMPUTE RESPONSIBILITY
-		blk_os = blockIdx.x*%(N)s/blockDim.x + threadIdx.x;
-		// Reads in old R, adds S & A, finds max of the sum
-		for (i=0; i<%(N)s; i+=blockDim.x) {
-			j = i + threadIdx.x;
-			old[j] = R[blk_os + j];
-			float AS_priv = S[blk_os + j] + A[blk_os + j];
-			atomicMax(&atom_temp, AS_priv);
-			AS[j] = AS_priv;
-		}
-		__syncthreads();
-
-		// Updates responsibility, sets the max to -infinity
-		for (i=0; i<%(N)s; i+=blockDim.x) {
-			j = i + threadIdx.x;
-			R[blk_os + j] = S[blk_os + j] - atom_temp;
-			if (AS[j] == atom_temp) {
-				AS[j] = __int_as_float(0xff800000); //-infinity
-				max_idx = j;
-			}
-		}
-		__syncthreads();
-
-		// Find new max
-		for (i=0; i<%(N)s; i+=blockDim.x) {
-			atomicMax(&atom_temp, AS[i + threadIdx.x]);
-		}
-		__syncthreads();
-
-		// Update responsibility again at the max index only
-		if (threadIdx.x == 0)
-			R[blk_os + max_idx] = S[blk_os + max_idx] - atom_temp;
-		__syncthreads();
-
-		// Apply damping
-		for (i=0; i<%(N)s; i+= blockDim.x) {
-			j = i + threadIdx.x;
-			temp = (1-%(DAMP)s)*R[blk_os + j] + %(DAMP)s*old[j];
-			if (temp > FLT_MAX)
-				R[blk_os + j] = FLT_MAX;
-			else
-				R[blk_os + j] = temp;
-		}
-		__syncthreads();
-
-		// COMPUTE AVAILABILITY
-		// Note: indexing is transposed
-
-		// Read in old A, find elementwise max of R, 0
-		j = threadIdx.x*%(N)s;
-		for (i=0; i<%(N)s; i+=blockDim.x) {
-			blk_os = i*%(N)s + blockIdx.x;
-			k = i + threadIdx.x;
-			old[k] = A[blk_os + j];
-			if (R[blk_os + j] > 0) //elementwise max
-				Rp[k] = R[blk_os + j];
-			else
-				Rp[k] = 0;
-			atomicAdd(&atom_temp, Rp[k]); //sum Rp
-		}
-		__syncthreads();
-
-		// Update availability
-		for (i=0; i<%(N)s; i+=blockDim.x) {
-			blk_os = i*%(N)s + blockIdx.x;
-			k = i + threadIdx.x;
-			A[blk_os + j] = atom_temp - Rp[k];
-		}
-		__syncthreads();
-
-		// Grab diagonal
-		if (threadIdx.x == 0)
-			temp = A[blk_Idx.x*%(N)s + blockIdx.x];
-		__syncthreads();
-
-		// Set A to elementwise min of A, 0
-		for (i=0; i<%(N)s; i+=blockDim.x) {
-			blk_os = i*%(N)s + blockIdx.x;
-			if (A[blk_os + j] > 0)
-				A[blk_os + j] = 0;
-		}
-		__syncthreads();
-
-		// Replace diagonal
-		if (threadIdx.x == 0)
-			A[blockIdx.x*%(N)s + blockIdx.x] = dA;
-		__syncthreads();
-
-		// Apply damping
-		for (i=0; i<%(N)s; i+= blockDim.x) {
-			blk_os = i*%(N)s + blockIdx.x;
-			k = i + threadIdx.x;
-			temp = (1-%(DAMP)s)*A[blk_os + j] + %(DAMP)s*old[k];
-			A[blk_os + j] = temp;
-		}
-		__syncthreads();
-
-		// DONE - NEXT KERNEL CHECKS FOR CONVERGENCE
-		// SHIT - WE NEED TO SUM E = (diag(A) + diag(R)) > 0
-		// kernel is called again if unconverged
-}
 
 // Check for convergence on the diagonal of A & R
 // Use 1 block of 1024 threads
 // Note the converged flag only tells CPU to stop iterating.
 // CPU will not call this if MAXITS hit, so maxits checks are removed
-__global__ void convergence(float* A, float* R, int* E, bool *e, int iteration, bool converged) {
+__global__ void convergence(float* A, float* R, bool* E, int* e, int* se, int iteration, bool converged) {
 	unsigned int i;
 	unsigned int j;
-	unsigned int k;
+	unsigned bool E_priv;
+	unsigned int e_sum = 0;
 	unsigned int temp = 0;
-	float RA;
-	__shared__ bool E[%(N)s];
-	__shared__ int K; //sum of E
-	__shared__ int se[%(N)s];
-	__shared__ int conv_sum;
+	__shared__ int K[1]; //sum of E
+	__shared__ int conv_sum[1];
 
-	// E is vector of whether diagonal (A+R) > 0
+	if (threadIdx.x == 0) {
+		K[0] = 0;
+		conv_sum[0] = 0;
+	}
+	__syncthreads();
+
+	// E is bool vector of whether diagonal (A+R) > 0
 	for (i=0; i<%(N)s; i+=blockDim.x) {
 		j = i + threadIdx.x;
-		RA = A[j*%(N)s + j] + R[j*%(N)s + j];
-		if (RA > 0) E[j] = true;
-		else E[j] = false;
-		e[((iteration-1) % %(CONVITS)s)*%(N)s + j] = E[j];
-		atomicAdd(&K, E[j]);
+		E_priv = (A[j] + R[j]) > 0;
+		e[((iteration-1) % %(CONVITS)s)*%(N)s + j] = (int)E_priv;
+		E[j] = E_priv;
+		atomicAdd(&K[0], (int)E_priv);
 	}
 	__syncthreads();
 
@@ -308,19 +192,15 @@ __global__ void convergence(float* A, float* R, int* E, bool *e, int iteration, 
 		for (i=0; i<%(N)s; i+=blockDim.x) {
 			j = i + threadIdx.x;
 			for (k=0; k<%(CONVITS)s; k++)
-				temp += e[k*%(N)s + j];
-			se[j] = temp;
-			temp = 0;
-			if (se[j] == %(CONVITS)s) temp++;
-			if (se[j] == 0) temp++;
-			atomicAdd(&conv_sum, temp);
+				e_sum += e[k*%(N)s + j];
+			se[j] = e_sum;
+			temp += (int)((e_sum == %(CONVITS)s) + (e_sum == 0));
 		}
+		atomicAdd(&conv_sum[0], temp);
 		__syncthreads();
-		if (conv_sum == N && K > 0 && threadIdx.x == 0)
-			converged = true;
+		converged = (conv_sum[0] == %(N)s) && (K[0] > 0) && (threadIdx.x == 0);
 	}
 }
-*/
 """
 
 N = 32
@@ -333,5 +213,4 @@ similarity = mod.get_function("similarity")
 preference = mod.get_function("preference")
 responsibilities = mod.get_function("responsibilities")
 availabilities = mod.get_function("availabilities")
-#apupdate = mod.get_function("apupdate")
-#convergence = mod.get_function("convergence")
+convergence = mod.get_function("convergence")
--- src/test_kernel.py
+++ src/test_kernel.py
@@ -37,64 +37,111 @@ for i in range(N):
 	sim[i,i] = pref
 print "Validation: ", np.allclose(sim, sim_cpu)
 
-# TEST RESPONSIBILITY KERNEL
-
-j = 0;  #1st row
+# CONSTANTS
+#j = 0;  #1st row
 DAMPFACT = 0.9
+CONVITS = 100
+MAXITS = 1000
+
+
+dn = 0
+i = 0
+
 A = np.zeros((N,N), np.float32)
 R = np.zeros((N,N), np.float32)
-S_gpu = ker.gpuarray.to_gpu(sim_cpu[j,:])
-A_gpu = ker.gpuarray.to_gpu(A[j,:])
-R_gpu = ker.gpuarray.to_gpu(R[j,:])
-AS_gpu = ker.gpuarray.zeros(R[j,:].shape, np.float32)
-
-#CPU
-
-ss = sim_cpu[j,:] # get all s(i,k)
-a_s = A[j,:] + ss # compute a(i,k) + s(i,k)
-Y = np.max(a_s).astype(np.float32) # get the max of a(i,k) + s(i,k)
-I = np.argmax(a_s)
-a_s[I] = np.finfo('float32').min # for r(i,k) where max(a+s) occurs at (i,k), need to find the next maximum that occurs (see eqn) so that the max occurs at (i,k') s.t. k != k'
-Y2 = np.max(a_s).astype(np.float32) # find the next max
-I2 = np.argmax(a_s)
-r = ss - Y # do s(i,k) - max(a+s)
-r[I] = ss[I] - Y2 # replace w/ s(i,k) - max(a(i,k')+s(i,k')), k'!=k if max(a+s) was at (i,k)
-R[j,:] = (1-DAMPFACT) * r + DAMPFACT * R[j,:]  # dampen
-
-# GPU
-ker.responsibilities(S_gpu, R_gpu, A_gpu, AS_gpu,
-	grid=(j+1,1,1),
-	block=(N,1,1))
-R_cpu = R_gpu.get()
-A_cpu = A_gpu.get()
-print R_cpu
-print A_cpu
-print "Validation: ", np.allclose(R[j,:], R_cpu)
-print "Validation: ", np.allclose(A[j,:], A_cpu)
-#print R[j,:]
-#print A[j,:]
-
-# TEST AVAILABILITY KERNEL
+A_cpu = np.zeros((N,N), np.float32)
+R_cpu = np.zeros((N,N), np.float32)
+AS_gpu = ker.gpuarray.zeros((N,N)), np.float32)
 RP = np.zeros((N,N), np.float32)
-A_gpu = ker.gpuarray.to_gpu(A[:,j])
-R_gpu = ker.gpuarray.to_gpu(R[:,j])
-RP_gpu = ker.gpuarray.to_gpu(RP[:,j])
+RP_gpu = ker.gpuarray.zeros((N,N), np.float32)
+E = np.zeros(N, np.bool)
+E_cpu = np.zeros(N, np.bool)
+e_gpu = ker.gpuarray.zeros((CONVITS, N), np.int32)
+se_gpu = ker.gpuarray.zeros(N, np.int32)
+converged_gpu = ker.gpuarray.zeros(1, np.bool)
 
-#CPU
-rp = np.maximum(R[:,j], 0) # elementwise maximum of r transposed
-rp[j] = R[j,j] # replace r(k,k) which is not subject to max
-a = np.sum(rp) - rp # a(k,k) = sum(max{0,r(i',k)}) s.t. i'!=k, else = sum(max{0,r(i',k)}) + r(k,k) - r(i,k), i'!=k which is equivalent to sum(max{0,r(i'k)}) + r(k,k) for i'!=i,k
-dA = a[j] # grab a(k,k) which is not subject to min
-a = np.minimum(a, 0) # elementwise minimum for a(i,k)
-a[j] = dA # replace a(k,k)
-A[:,j] = (1-DAMPFACT) * a + DAMPFACT * A[:,j] # dampen
+while not dn and i < MAXITS:
+	i += 1
 
-#GPU
-iteration = 0
-ker.availabilities(A_gpu, R_gpu, RP_gpu, np.int32(iteration),
-	grid=(j+1,1,1),
-	block=(N,1,1))
-A_cpu = A_gpu.get()
-print A_cpu
-print A[:,j]
-print "Validation: ", np.allclose(A[:,j], A_cpu)
+	# TEST RESPONSIBILITY KERNEL
+	S_gpu = ker.gpuarray.to_gpu(sim_cpu)
+	A_gpu = ker.gpuarray.to_gpu(A_cpu)
+	R_gpu = ker.gpuarray.to_gpu(R_cpu)
+
+	#CPU
+	for j in range(N):
+		ss = sim_cpu[j,:] # get all s(i,k)
+		a_s = A[j,:] + ss # compute a(i,k) + s(i,k)
+		Y = np.max(a_s).astype(np.float32) # get the max of a(i,k) + s(i,k)
+		I = np.argmax(a_s)
+		a_s[I] = np.finfo('float32').min # for r(i,k) where max(a+s) occurs at (i,k), need to find the next maximum 	that 	occurs (see eqn) so that the max occurs at (i,k') s.t. k != k'
+		Y2 = np.max(a_s).astype(np.float32) # find the next max
+		I2 = np.argmax(a_s)
+		r = ss - Y # do s(i,k) - max(a+s)
+		r[I] = ss[I] - Y2 # replace w/ s(i,k) - max(a(i,k')+s(i,k')), k'!=k if max(a+s) was at (i,k)
+		R[j,:] = (1-DAMPFACT) * r + DAMPFACT * R[j,:]  # dampen
+
+	# GPU
+	ker.responsibilities(S_gpu, R_gpu, A_gpu, AS_gpu,
+		grid=(N,1,1),
+		block=(N,1,1))
+	R_cpu = R_gpu.get()
+	A_cpu = A_gpu.get()
+	#print R_cpu
+	#print A_cpu
+	print "R Validation: ", np.allclose(R, R_cpu)
+	#print "A Validation: ", np.allclose(A, A_cpu)
+	#print R[j,:]
+	#print A[j,:]
+
+	# TEST AVAILABILITY KERNEL
+	A_gpu = ker.gpuarray.to_gpu(A_cpu.T.copy())
+	R_gpu = ker.gpuarray.to_gpu(R_cpu.T.copy())
+
+	#CPU
+	for j in range(N):
+		rp = np.maximum(R[:,j], 0) # elementwise maximum of r transposed
+		rp[j] = R[j,j] # replace r(k,k) which is not subject to max
+		a = np.sum(rp) - rp # a(k,k) = sum(max{0,r(i',k)}) s.t. i'!=k, else = sum(max{0,r(i',k)}) + r(k,k) - r(i,k), i'!=k 	which is equivalent to sum(max{0,r(i'k)}) + r(k,k) for i'!=i,k
+		dA = a[j] # grab a(k,k) which is not subject to min
+		a = np.minimum(a, 0) # elementwise minimum for a(i,k)
+		a[j] = dA # replace a(k,k)
+		A[:,j] = (1-DAMPFACT) * a + DAMPFACT * A[:,j] # dampen
+
+	#GPU
+	iteration = 0
+	ker.availabilities(A_gpu, R_gpu, RP_gpu, np.int32(iteration),
+		grid=(N,1,1),
+		block=(N,1,1))
+	A_cpu = A_gpu.get()
+	#print A_cpu
+	#print A
+	print "A Validation: ", np.allclose(A, A_cpu)
+
+	# TEST CONVERGENCE KERNEL
+	E_gpu = ker.gpuarray.to_gpu(E_cpu)
+	A_gpu = ker.gpuarray.to_gpu(np.diag(A_cpu))
+	R_gpu = ker.gpuarray.to_gpu(np.diag(R_cpu))
+
+	#CPU
+    for j in range(N):
+        E[j] = (A[j,j] + R[j,j]) > 0 # Find where A(i,i)+R(i,i) is > 0 (i.e. find the exemplars)
+    e[(i-1) % CONVITS , :] = E # Buffer for convergence iterations
+    K = np.sum(E).astype(np.int32) # How many exemplars are there?
+    if i >= CONVITS or i>= MAXITS:
+        se = np.sum(e, 0).astype(np.int32) # Sum all convergence iterations
+        unconverged = np.sum((se==CONVITS) + (se==0)) != N # Unconverged if # of exemplars isn't same for CONVITS
+        if (not unconverged and K>0) or (i==MAXITS): # Stop the message passing loop
+            dn=1
+
+	#GPU
+	ker.convergence(A_gpu, R_gpu, E_gpu, e_gpu, se_gpu, np.int32(i), converged_gpu,
+		grid = (1,1,1),
+		block = (N,1,1))
+	E_cpu = E_gpu.get()
+	converged_cpu = converged_gpu.get()
+
+print E_cpu
+print E
+print "E Validation", np.allclose(E, E_cpu)
+print dn, converged_cpu
